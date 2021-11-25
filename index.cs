b1878c5b71d99345e198b2bf2a4a1593195f1abb
aanu1234cls <tunsabaze01@gmail.com>
2021-11-25 22:08:46 +0100

    first commit

---
+++ index.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace BoilerPlate {
+
+    class FramePlate {
+
+        // Global var...
+
+        static void Main( string[] args ) {
+            Console.WriteLine('Welcome to Frameplate application in CSharp.');
+            MyName();
+        }
+
+        string MyName() {
+            Console.WriteLine('Please, enter your name: ');
+
+            string userName = Console.ReadLine();
+            Console.WriteLine('Welcome! Your name is ' + userName);
+        }
+    }
+}
